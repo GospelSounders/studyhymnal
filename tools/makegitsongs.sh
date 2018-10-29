c9180178fca6a9ba7fcf16e22a3fa24168d84432
Brian Onang'o <surgbc@gmail.com>
2018-10-29 20:03:30 +0300

    init

--- tools/makegitsongs.sh
+++ tools/makegitsongs.sh
@@ -11,6 +11,7 @@ cd gitsongs
 
 for file in *.txt; do mv "$file" "${file/.txt/.md}"; done
 for file in *.md; do sed -i ':a;N;$!ba;s/\n/\n\n/g' $file; done
+for file in *.md; do sed -i ':a;N;$!ba;s/Refrain\n/# Refrain\n/g' $file; done
 find ./ -type f -exec sed -i "s/\([0-9]\)$/# \0/g"  {} \; #stanza numbers
 find ./ -type f -exec sed -i '1 s/^/# /' {} \;  # Song number
 find ./ -type f -exec sed -i '$s/$/\n\n[⬅️ Back to index](..\/README.md)/' {} \; #at the end

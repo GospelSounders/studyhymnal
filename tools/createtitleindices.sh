0131a1fc37b463d35174391ee0083f284e058593
Brian Onang'o <surgbc@gmail.com>
2018-10-29 20:01:23 +0300

    init

--- tools/createtitleindices.sh
+++ tools/createtitleindices.sh
@@ -10,54 +10,24 @@ ALLNUMBERS=()
 index=0;
 
 for i in `find ./`; do
-#   sed -e "s/[^0-9]//g" $i;
-# echo $i
-THIRDLINE=$(sed -n '3{p;q}' $i)
-SONGNUMBER=$(echo $THIRDLINE | sed -e "s/[^0-9]//g")
-# echo $THIRDLINE
-# echo $SONGNUMBER
-SONGTITLE=$(echo $THIRDLINE | cut -d' ' -f4-)
+    THIRDLINE=$(sed -n '3{p;q}' $i)
+    SONGNUMBER=$(echo $THIRDLINE | sed -e "s/[^0-9]//g")
+    SONGTITLE=$(echo $THIRDLINE | cut -d' ' -f4-)
 
-# remove leading zeros
-SONGNUMBERSHORT=$(echo $SONGNUMBER | sed 's/^0*//')
-if [ !  -z  $SONGNUMBERSHORT  ]; then
-    # echo $SONGNUMBERSHORT'->'$SONGTITLE 
-    TMP="$SONGNUMBER->$SONGTITLE"
-    TMP=$(echo $TMP| sed -e s/" "/_/g)
-    ALLTITLES+=( $TMP )
-    ALLTITLES[$index]=$TMP
-    # index=$index+1
-    let "index++"
-    echo $index
-    ALLNUMBERS+=( $SONGNUMBERSHORT )
-    # echo $SONGNUMBER'->'$SONGTITLE >> tmp.txt
-fi 
+    # remove leading zeros
+    SONGNUMBERSHORT=$(echo $SONGNUMBER | sed 's/^0*//')
+    if [ !  -z  $SONGNUMBERSHORT  ]; then
+        TMP="$SONGNUMBER->$SONGTITLE"
+        TMP=$(echo $TMP| sed -e s/" "/_/g)
+        ALLTITLES+=( $TMP )
+        ALLTITLES[$index]=$TMP
+        let "index++"
+        echo $index
+        ALLNUMBERS+=( $SONGNUMBERSHORT )
+    fi 
 done
 
-
-# printf '%s\n' "${ALLTITLES[@]}"
-
-# arr2=($(echo ${ALLTITLES[*]}| tr "....||" "\n" | sort -z))
-
 cd ../
-cat README.md
-# printf '%s\n' "${arr2[@]}"
-
-# # echo ${arr2[*]}
-
-# echo "...................................."
-# echo ${ALLTITLES[0]}
-# echo ${ALLTITLES[1]}
-# echo ${ALLTITLES[2]}
-# echo ${ALLTITLES[3]}
-
-# echo ${arr2[0]}
-# echo ${arr2[1]}
-# echo ${arr2[2]}
-# echo ${arr2[3]}
-
-# printf '%s\n' "${ALLTITLES[@]}" | sort -n
-echo "...................................."
 
 readarray -td '' SORTEDTITLES < <(printf '%s\0' "${ALLTITLES[@]}" | sort -z)
 printf '%s\n' "${SORTEDTITLES[@]}"
@@ -66,7 +36,7 @@ line=$(grep -n '## Index of Titles' README.md | cut -d: -f 1)
 let "line+=2"
 
 cp README.template.md README.md 
-# echo $line
+
 for i in "${SORTEDTITLES[@]}"; do
     arrIN=(${i//->/ })
     SONGNUMBER=${arrIN[0]}
@@ -78,23 +48,8 @@ for i in "${SORTEDTITLES[@]}"; do
     # echo "$SONGNUMBERSHORT=>$SONGTITLE"
     sed -i "$line a $SONGNUMBERSHORT  | [$SONGTITLE](gitsongs/$SONGNUMBER.md)" README.md
 
-    
-    # HEADER=$(cat songheaders/$SONGNUMBER.md 2>/dev/null )
-
-    # headerline=5
-    # if [ !  -z  "$HEADER"  ]; then
-    # # if [ !  -z  $header ]; then
-    #    sed -i "$headerline a \$header" gitsongs/$SONGNUMBER.md 
-
-    #    sed "5r songheaders/$SONGNUMBER.md" < gitsongs/$SONGNUMBER.md  > tempFile.txt
-    # mv tempFile.txt gitsongs/$SONGNUMBER.md 
-    # fi 
-
-    sed "5r songheaders/$SONGNUMBER.md" < gitsongs/$SONGNUMBER.md  > tempFile.txt
+    sed "4r songheaders/$SONGNUMBER.md" < gitsongs/$SONGNUMBER.md  > tempFile.txt
     mv tempFile.txt gitsongs/$SONGNUMBER.md 
-    
-    
-    # pwd
-    # echo $header
+ 
     let "line++"
 done
--- tools/makegitsongs.sh
+++ tools/makegitsongs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ROOTDIR=$(pwd)
+
 DIR=$1
 cd $DIR
 pwd
@@ -8,7 +10,15 @@ cp -r songs gitsongs
 cd gitsongs
 
 for file in *.txt; do mv "$file" "${file/.txt/.md}"; done
+for file in *.md; do sed -i ':a;N;$!ba;s/\n/\n\n/g' $file; done
 find ./ -type f -exec sed -i "s/\([0-9]\)$/# \0/g"  {} \; #stanza numbers
 find ./ -type f -exec sed -i '1 s/^/# /' {} \;  # Song number
 find ./ -type f -exec sed -i '$s/$/\n\n[⬅️ Back to index](..\/README.md)/' {} \; #at the end
 find ./ -type f -exec sed -i '1 s/^/[⬅️ Back to index](..\/README.md)\n\n/' {} \; #at the begining
+
+
+
+
+cd $ROOTDIR
+
+./createtitleindices.sh "$DIR/gitsongs"
